74ca9b906c738235fb736fee00f2abbeec48b6e3
Nidaiq <nida.iqbal@gmail.com>
2021-03-11 16:33:41 -0500

    Adding PyPoll.py file

---
+++ python_practice.py
@@ -0,0 +1,7 @@
+groceryList=["Milk","Bread","Eggs","Peanut Butter","Jelly"]
+print(groceryList)
+groceryList[3]="Almond Butter"
+groceryList.remove("Jelly")
+groceryList.append("coffee")
+print(groceryList)
+print(groceryList[0])
