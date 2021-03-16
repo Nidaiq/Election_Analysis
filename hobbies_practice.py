5cef306edad044ca37fa99f6da1181f5b9ba4c4b
Nidaiq <nida.iqbal@gmail.com>
2021-03-16 12:37:30 -0400

    final files for challenge

--- hobbies_practice.py
+++
@@ -1,8 +0,0 @@
-mypet = {"name":"cat",
-        "age":3,
-        "hobbies":["meow","eat","sleep"],
-        "wakeup":{"monday":7, "tuesday":7,"wednesday":7}}
-print(f'Hello my name is {mypet["name"]} and I am {mypet["age"]} years old')
-print(f'I have {len(mypet["hobbies"])} hobbies')
-print(f'my favorite hobby is {mypet["hobbies"][2]}')
-print(f'on monday i wake up at {mypet["wakeup"]["tuesday"]}')
--- python_practice.py
+++
@@ -1,7 +0,0 @@
-groceryList=["Milk","Bread","Eggs","Peanut Butter","Jelly"]
-print(groceryList)
-groceryList[3]="Almond Butter"
-groceryList.remove("Jelly")
-groceryList.append("coffee")
-print(groceryList)
-print(groceryList[0])
